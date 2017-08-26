8488e42b64b4a5c956018e51faae4fdc9460e17c
SnakeofStone <jorgeruben99@gmail.com>
2017-08-26 18:18:24 -0500

    Added the assets folder

--- headers/Nave.hpp
+++ headers/Nave.hpp
@@ -1,1 +1,9 @@
+#include <SFML/Graphics.hpp>
 
+static class Nave
+{
+private:
+    int posX;
+public:
+    Nave();
+}
--- source/Nave.cpp
+++ source/Nave.cpp
@@ -1,1 +1,12 @@
+#include "Nave.hpp"
 
+Nave::Nave()
+{
+    sf::Texture nave;
+    nave.loadFromFile("assets/");
+}
+
+Nave::MovePosX(int x)
+{
+    posX += x;
+}
